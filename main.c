452b9cba8ee1185ce47df36fb187718fd98e851d
Frank Theile <frank.theile@web.de>
2018-10-07 23:03:48 +0200

    Test sp_list_ports() and sp_get_port_name()

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <libserialport.h>
 
+struct client {
+	struct sp_port** port_list;
+};
+
+static struct client theApp;
+
 int main(int argc, char* argv[])
 {
+	if (sp_list_ports(&theApp.port_list) == SP_OK) {
+		int i = 0;
+		struct sp_port* port;
+		do {
+			port = theApp.port_list[i];
+			if (port) {
+				const char* name = sp_get_port_name(port);
+				if (name) {
+					printf("%s\n", name);
+				}
+			}
+			i++;
+		} while (port);
+		sp_free_port_list(theApp.port_list);
+	} else {
+		printf("sp_list_ports() error\n");
+	}
 	return 0;
 }
