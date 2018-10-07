38386253f9828f2cd49c87b7ebd7f101dc6baf9e
Frank Theile <frank.theile@mailbox.org>
2018-10-07 23:29:24 +0200

    Refactoring: use iterator instead of index

--- main.c
+++ main.c
@@ -11,18 +11,14 @@ int main(int argc, char* argv[])
 {
 	enum sp_return ret = sp_list_ports(&theApp.port_list);
 	if (ret == SP_OK) {
-		int i = 0;
-		struct sp_port* port;
-		do {
-			port = theApp.port_list[i];
-			if (port) {
-				const char* name = sp_get_port_name(port);
-				if (name) {
-					printf("%s\n", name);
-				}
+		struct sp_port** port_iter = theApp.port_list;
+		while (*port_iter) {
+			const char* name = sp_get_port_name(*port_iter);
+			if (name) {
+				printf("%s\n", name);
 			}
-			i++;
-		} while (port);
+			port_iter++;
+		}
 		sp_free_port_list(theApp.port_list);
 	} else {
 		printf("sp_list_ports(): %d\n", ret);
